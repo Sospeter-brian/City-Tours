de60f688e2343f17af5adf80a076c7e0e6e13be1
sospeterbrian029@gmail.com <sospeterbrian029@gmail.com>
2022-07-27 11:43:53 +0300

    first steps

---
+++ city-tour/src/App.js
@@ -0,0 +1,12 @@
+import React,{useState, useEffect} from 'react';
+import Loading from "./Loading"
+import Tours from "./Tours";
+
+const url = "api url"
+function App() {
+  return (
+    <h1>Hello World</h1>
+  )
+}
+
+export default App;
